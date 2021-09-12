0b864efe79e72ed4aeadcbea93f006873fc4db07
Cecep Muhammad Ridwan <88409866+ibnahmad@users.noreply.github.com>
2021-09-12 16:44:57 +0700

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,75 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    
+  
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+
+    <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="./rest-api/source-code-rest-api/img/logo.png" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-3">
+        <a class="nav-link" aria-current="page" href="#">All Menu</a>
+        <a class="nav-link" href="#">Pizza</a>
+        <a class="nav-link" href="#">Pasta</a>
+        <a class="nav-link" href="#">Nasi</a>
+        <a class="nav-link" href="#">Minuman</a>
+      </div>
+    </div>
+  </div>
+    </div>
+</nav>
+
+
+
+
+
+
+  <div class="container">
+
+
+    <div class="row mt-3">
+      <div class="col">
+        <h1>All Menu</h1>
+      </div>
+      <div class="row" id="daftar-menu">
+
+    
+      </div>
+
+    </div>
+
+
+  
+
+
+
+
+
+  </div>
+
+  
+      <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+      <script src="./script.js"></script>
+      
+  </body>
+</html>
---
+++ project/script.js
@@ -0,0 +1,44 @@
+
+function tampilkanAll(){
+$.getJSON('./burger.json', function(data){
+
+	let menu = data.menu;
+
+	$.each(menu, function(i, data){
+
+		$('#daftar-menu').append('<div class="col-md-4"><div class="card"><img src="./rest-api/source-code-rest-api/img/pizza/'+data.gambar+'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+data.nama+'</h5><p class="card-text">'+data.deskripsi+'</p><h5 class="card-title">Rp.'+data.harga+'</h5><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>')
+});
+});
+}
+	
+
+
+		$('.nav-link').on('click', function(){
+
+			$('.nav-link').removeClass('active');
+			$(this).addClass('active');
+
+			let kategori = $(this).html();
+			$('h1').html(kategori);
+			
+			if(kategori == 'All Menu') {
+				tampilkanAll();
+				return;
+			}
+		$.getJSON('./burger.json', function(data){
+				let menu = data.menu;
+				let content ="";
+
+				$.each(menu, function(i, data){
+
+					if(data.kategori == kategori.toLowerCase()){
+						content += '<div class="col-md-4"><div class="card"><img src="./rest-api/source-code-rest-api/img/pizza/'+data.gambar+'" class="card-img-top"><div class="card-body"><h5 class="card-title">'+data.nama+'</h5><p class="card-text">'+data.deskripsi+'</p><h5 class="card-title">Rp.'+data.harga+'</h5><a href="#" class="btn btn-primary">Go somewhere</a></div></div></div>'
+					}
+				});
+				$('#daftar-menu').html(content);
+
+			})
+		})
+
+
+
